62965dc9b1b7886bc68be20c4ddda2bef21abea1
ldianwu <ldianwu@qq.com>
2019-06-15 11:05:59 +0800

    增加chapter_201906工程

---
+++ chapter_201906/src/main/java/com/lidianwu/alglearning/FirstUniqChar.java
@@ -0,0 +1,45 @@
+package com.lidianwu.alglearning;
+
+/**
+ * 字符串中的第一个唯一字符
+ *
+ * @author ldianwu
+ * @date 2019-06-14
+ */
+public class FirstUniqChar {
+
+    /**
+     * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+     *
+     * @param s
+     * @return
+     */
+    public static int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        char[] chars = s.toCharArray();
+        for (char ch : chars) {
+            freq[ch - 'a']++;
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (freq[chars[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int firstUniqChar_1(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (s.indexOf(ch) == s.lastIndexOf(ch)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String s = "leetcode";
+        System.out.println(firstUniqChar_1(s));
+    }
+}
